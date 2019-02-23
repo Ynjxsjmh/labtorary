f5db56f947bd181544c2dcfc1e0a43fc23a7f17c
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-02-23 15:37:48 +0800

    create repo

---
+++ Cpp/fwrite/write_array.cpp
@@ -0,0 +1,22 @@
+/* fwrite example : write buffer */
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    FILE * pFile;
+    char buffer[] = { 'x' , 'y' , 'z' };
+    pFile = fopen ( "write_buffer.out" , "wb" );
+    fwrite(buffer , sizeof(char), sizeof(buffer) , pFile );
+    fclose(pFile);
+
+	cout<<sizeof(buffer)<<endl;
+
+	char* buf;
+    pFile = fopen ( "write_buffer.out" , "rb" );
+    fread(buf, sizeof(char), sizeof(buffer), pFile);
+    
+    cout<<buf;
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+## About
+Unknown function test.
