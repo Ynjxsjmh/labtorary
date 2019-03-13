ad27af0b67eb075d078db4037acb27b3f62a965d
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-03-13 18:10:25 +0800

    Test string variable element access

---
+++ Cpp/string/element_access.cpp
@@ -0,0 +1,11 @@
+#include <cstring>
+#include <iostream>
+
+int main() {
+    std::string a = "This is a test";
+
+    // It's ok and will output T
+    std::cout << a[0] << std::endl;
+
+    return 0;
+}
