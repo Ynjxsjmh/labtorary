a5bc83fd3a35d95435fb09bda633d64e6dcc1cf1
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-10-14 10:45:47 +0800

    [PowerShell] Change specific file name

---
+++ PowerShell/changFileName.ps1
@@ -0,0 +1,15 @@
+﻿# 更改文件名 “猥琐萌**” 至 “***”
+$path = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent #获取当前目录
+$directory = $path + "\rename"
+$files = Get-ChildItem -Path $path -File *.png  # 获取当前文件夹下所有 png 格式的文件
+foreach ($file in $files) {
+    $fileName = $file.Name[3..($file.Name.length-1)]  # 去掉文件中“猥琐萌”三个字
+
+    $convertNameType = [System.String]$fileName -replace ' ', ''  # System.String 强制转化 System.Object[] to System.String
+
+    $file.FullName
+    $fileName.ToString()
+    $fileName.GetType()  # 得到变量类型
+
+    Rename-Item -Path $file.FullName -NewName $convertNameType
+}
